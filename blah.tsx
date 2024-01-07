7b537665048abf083442e5ffcff4abaabdf56a3f
DexFerrell <dexferrell@outlook.com>
2024-01-07 12:34:39 -0500

    new

---
+++ blah.tsx
@@ -0,0 +1,13 @@
+export const footerLinks = [
+  {
+    title: 'For developers',
+    links: [
+      'Go Pro!',
+      'Explore development work',
+      'Development blog',
+      'Code podcast',
+      'Open-source projects',
+      'Refer a Friend',
+      'Code of conduct',
+    ],
+  }, 
